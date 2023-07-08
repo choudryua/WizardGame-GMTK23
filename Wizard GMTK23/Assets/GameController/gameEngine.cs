e55c4babbbdc3b8144aba0946f9e69c43c16939b
Feargem <censinggem101@gmail.com>
2023-07-07 21:00:31 -0600

    dxcSZXC

--- Wizard GMTK23/Assets/GameController/gameEngine.cs
+++ Wizard GMTK23/Assets/GameController/gameEngine.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameEngine : MonoBehaviour
+public class GameEngine : MonoBehaviour
 {
     private bool isPaused;
+    public bool isMainMenu;
+    public Pause pauseManager;
     // Start is called before the first frame update
     void Start()
     {
         isPaused = true;
-
+        pauseManager.PauseGame();
         InputHandler.instance.OnJumpPressed += args => OnMenuPressed();
         InputHandler.instance.OnJumpPressed += args => OnPlayerPressed();
     }
@@ -19,18 +21,25 @@ public class gameEngine : MonoBehaviour
     {
     }
 
+    public void GameStart()
+    {
+        isPaused = false;
+        pauseManager.UnPauseGame();
+    }
     private void OnMenuPressed()
     {
-        if (isPaused) 
-        { 
+        if (isPaused && !isMainMenu) 
+        {
+            pauseManager.OnPauseMenu();
             isPaused = false;
         }
     }
 
     private void OnPlayerPressed()
     {
-        if (!isPaused)
+        if (!isPaused && !isMainMenu)
         {
+            pauseManager.UnPauseMenu();
             isPaused = true;
         }
     }
