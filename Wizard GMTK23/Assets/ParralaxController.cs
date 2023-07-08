364c2304b63770f5a058ffb28c39fc193ea05266
choudryua <67034102+choudryua@users.noreply.github.com>
2023-07-08 06:48:57 -0400

    commit

---
+++ Wizard GMTK23/Assets/ParralaxController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParralaxController : MonoBehaviour
+{
+    Vector3 startPosition;
+    public Camera cam;
+    public float modifier;
+    float width;
+    void Start()
+    {
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPosition = transform.position;
+    }
+    void Update()
+    {
+        Vector3 camOffset = cam.transform.position * modifier;
+        transform.position = startPosition + new Vector3(camOffset.x, camOffset.y, 0);
+        if ((cam.transform.position.x - transform.position.x) > width)
+        {
+            startPosition += new Vector3(width, 0, 0);
+        }
+        if ((cam.transform.position.x - transform.position.x) < -width)
+        {
+            startPosition -= new Vector3(width, 0, 0);
+        }
+    }
+}
