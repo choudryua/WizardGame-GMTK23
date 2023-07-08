210676efbc59651c73d07b452073f66e94b05b12
Feargem <censinggem101@gmail.com>
2023-07-08 02:52:01 -0600

    123123123

--- Wizard GMTK23/Assets/Player/Scripts/AiMovement.cs
+++ Wizard GMTK23/Assets/Player/Scripts/AiMovement.cs
@@ -1,11 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class AiMovement : MonoBehaviour
 {
-    public MovementController movementController;
+    [SerializeField]
+    private MovementController movementController;
+    [SerializeField]
+    private GameEngine gameEngine;
+    // pos sys
+    private Vector2 curDestinationPos;
+
+    [SerializeField]
+    private Vector2 destinationCheckSize;
+    [SerializeField]
+    private Transform destinationCheckTransform;
+    [SerializeField]
+    private LayerMask destinationLayer;
+    [SerializeField]
+    private string enemyTag;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +30,35 @@ public class AiMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Console.WriteLine(movementController._moveInput.ToString());
-        movementController._moveInput.x = 1;
+        if (Physics2D.OverlapBox(destinationCheckTransform.position, destinationCheckSize, 0, destinationLayer))
+        {
+            MoveCharacter(curDestinationPos);
+        }
+        
+    }
+
+    private void MoveCharacter(Vector2 destination)
+    {
+
+    }
+
+    private void LeftRightMovement(int x)
+    {
+        movementController._moveInput.x = x;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(enemyTag))
+        {
+            Console.WriteLine("oohnoo :3");
+                //gameEngine.reset level();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(destinationCheckTransform.position, destinationCheckSize);
     }
 }
