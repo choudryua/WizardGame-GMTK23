7e03f4456874d869700201b1012ce775be6f39e4
Feargem <censinggem101@gmail.com>
2023-07-07 18:30:06 -0600

    wddaw

---
+++ Wizard GMTK23/Assets/GameController/ChangeCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas thisCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Awake()
+    {
+        thisCanvas.worldCamera = FindAnyObjectByType<Camera>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
