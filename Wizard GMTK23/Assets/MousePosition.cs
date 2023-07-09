5a60e995f5a63449cd5624e6e4543e02c8098284
choudryua <67034102+choudryua@users.noreply.github.com>
2023-07-08 21:11:01 -0400

    commit

--- Wizard GMTK23/Assets/MousePosition.cs
+++ Wizard GMTK23/Assets/MousePosition.cs
@@ -5,11 +5,41 @@ using UnityEngine.InputSystem;
 
 public class MousePosition : MonoBehaviour
 {
-    public float moveSpeed = 10;
+
+    // FOLLOW DELAY
+    //public float moveSpeed = 10;
+    //void Update()
+    //{
+    //   Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+    //}
+
+
+
+
+    //SMOOTH DAMP
+    //public float maxMoveSpeed = 10;
+    //public float smoothTime = 0.3f;
+    //Vector2 currentVelocity;
+    //void Update()
+    //{
+    //    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+    //}
+
+
+    // FOLLOW AT A DISTANCE
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    public float minDistance = 2;
+    Vector2 currentVelocity;
     void Update()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+        // Offsets the target position so that the object keeps its distance.
+        mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
     }
+
 
 }
