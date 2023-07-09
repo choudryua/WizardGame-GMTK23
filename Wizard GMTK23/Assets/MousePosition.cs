d42f63b02c5af83be99673a598e9398a431b9b58
choudryua <67034102+choudryua@users.noreply.github.com>
2023-07-08 21:28:02 -0400

    move

--- Wizard GMTK23/Assets/MousePosition.cs
+++ Wizard GMTK23/Assets/MousePosition.cs
@@ -29,17 +29,33 @@ public class MousePosition : MonoBehaviour
 
 
     // FOLLOW AT A DISTANCE
-    public float maxMoveSpeed = 10;
-    public float smoothTime = 0.3f;
-    public float minDistance = 2;
-    Vector2 currentVelocity;
-    void Update()
+    //public float maxMoveSpeed = 10;
+    //public float smoothTime = 0.3f;
+    //public float minDistance = 2;
+    //Vector2 currentVelocity;
+    //voidUpdate()
+    //{
+    //   Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    // Offsets the target position so that the object keeps its distance.
+    //   mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
+    //   transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+    //}
+
+    public Rigidbody2D rb;
+    public float turnSpeed = 45;
+    public float moveSpeed = 5;
+    void FixedUpdate()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // Offsets the target position so that the object keeps its distance.
-        mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
-        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 direction = mousePosition - transform.position;
+
+        float angle = Vector2.SignedAngle(Vector2.right, direction);
+
+        Vector3 targetRotation = new Vector3(0, 0, angle);
+
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
+
+        rb.MovePosition(rb.position + ((Vector2)transform.right * moveSpeed * Time.deltaTime));
     }
-
-
 }
