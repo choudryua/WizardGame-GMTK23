6ab46f5739d7325657953874188a860ae0fa2e87
Feargem <censinggem101@gmail.com>
2023-07-13 19:36:57 -0600

    Update MousePosition.cs

--- Wizard GMTK23/Assets/_Scripts/MousePosition.cs
+++ Wizard GMTK23/Assets/_Scripts/MousePosition.cs
@@ -56,17 +56,24 @@ public class MousePosition : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        try
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector3 direction = mousePosition - transform.position;
+            Vector3 direction = mousePosition - transform.position;
 
-        float angle = Vector2.SignedAngle(Vector2.right, direction);
+            float angle = Vector2.SignedAngle(Vector2.right, direction);
 
-        Vector3 targetRotation = new Vector3(0, 0, angle);
+            Vector3 targetRotation = new Vector3(0, 0, angle);
 
-        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
+            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
 
-        rb.MovePosition(rb.position + ((Vector2)transform.right * lerpedValue * Time.deltaTime));
+            rb.MovePosition(rb.position + ((Vector2)transform.right * lerpedValue * Time.deltaTime));
+        }
+        catch(System.Exception e)
+        {
+            print(e.ToString());
+        }
     }
 
 
